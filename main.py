41a36b7c790de2cab5fe320a8507e1ddca5053dd
Rin Li <lijiaxu350@gmail.com>
2026-07-01 15:54:12 -0400

    Added route for update_server in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
+import git
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,5 +18,15 @@ def education():
 def contact():
     return render_template("contact.html", subtitle="Contact")
 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/jiaxuli350/portfolio_1')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+    
 if __name__ == '__main__':
     app.run(debug = True, host = "0.0.0.0")
